690ed5f6b74127922a142e1774ac09b9a044d578
mayi <mayi02@hnjing.com>
2018-12-25 14:35:46 +0800

    answer 011

---
+++ python2/ex11.py
@@ -0,0 +1,5 @@
+f = [0, 1]
+for i, v in enumerate(f[i-1] for i in range(2, int(input("x > ")) + 2) if not f.append(f[i - 1] + f[i - 2])):
+    print(format(v, ">12"), end='')
+    if not (i+1) % 6:
+        print()
