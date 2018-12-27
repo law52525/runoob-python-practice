542fc6215269ee399b71dde49114344e84d2806f
law52525 <law52526@qq.com>
2018-12-27 23:29:52 +0800

    add questions.md

---
+++ python2/questions.py
@@ -0,0 +1,12 @@
+import requests
+import re
+
+with open("questions.md", "w", encoding="utf-8") as f:
+    for i in range(1, 101):
+        h = requests.get("http://www.runoob.com/python/python-exercise-example{}.html".format(str(i)))
+        # 缺44题 两个 3 行 3 列的矩阵，实现其对应位置的数据相加，并返回一个新矩阵（不符合规则的例外）
+        r = re.search(r'<p><strong>题目：</strong>(.*?)</p>', h.content.decode("utf-8"), re.S)
+        if r:
+            s = "{:>3}. {}\n".format(i, r.group(1))
+            print(s)
+            f.write(s)
